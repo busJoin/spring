3f3ceba2a6cbe78409213384099e5938df2be541
busJoin <1934199858.com>
2019-11-16 11:11:41 +0800

    spring 生命周期

--- src/main/java/b/di/BookDaoImpl.java
+++ src/main/java/b/di/BookDaoImpl.java
@@ -4,4 +4,12 @@ public class BookDaoImpl implements  BookDao {
     public void addBook() {
         System.out.println("di add book");
     }
+
+    public  void myInit(){
+        System.out.println("初始化");
+    }
+
+    public void  myDestory(){
+        System.out.println("销毁");
+    }
 }
--- src/main/java/b/di/TestDI.java
+++ src/main/java/b/di/TestDI.java
@@ -12,14 +12,18 @@ public class TestDI {
     public void demo01(){
         //启动就会创建
         String xmlPath = "applicationContext.xml";
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
         BookServiceImpl bookService = (BookServiceImpl) applicationContext.getBean("bookServiceId");
         bookService.addBook();
+        //要求：1.容器必须close，销毁方法执行; 2.必须是单例的
+//		applicationContext.getClass().getMethod("close").invoke(applicationContext);
+        // * 此方法接口中没有定义，实现类提供
+        applicationContext.close();
     }
 
     @Test
     public void demo02(){
-        //使用BeanFactory  --第一次条用getBean实例化
+        //使用BeanFactory  --第一次调用getBean实例化
         String xmlPath = "applicationContext.xml";
         BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource(xmlPath));
         BookService bookService = (BookService) beanFactory.getBean("bookServiceId");
